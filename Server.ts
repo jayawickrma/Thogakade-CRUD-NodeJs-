afae1a36e66d3a9db0264be0599cc5f3ef793b7b
jayawickrama <nishantharaka2004@gmail.com>
2025-01-23 18:41:53 +0530

    api working successfully

--- Server.ts
+++ Server.ts
@@ -1,11 +1,12 @@
 import express from "express"
+import mainRouter from "./Routes/MainRoutes";
 const app=express()
 const port =3000;
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-app.use('/api/store',)
+app.use('/api/store',mainRouter.router)
 
 
 app.listen(port,()=>{
